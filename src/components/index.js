efce025d1f8180c4e357d21b5ff84b8873d0e4da
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-01-11 15:21:25 +0545

    feat: :sparkles: some more imports to the app

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,7 @@ export { default as ChannelDetail } from "./ChannelDetail";
 export { default as Feed } from "./Feed";
 export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
+export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
+export { default as Loader } from "./Loader";
