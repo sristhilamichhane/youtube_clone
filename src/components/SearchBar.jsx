dac7125bb604c35f9217a43954ea30731ac28beb
Sristhi Lamichhane <59720998+sristhilamichhane@users.noreply.github.com>
2023-01-15 09:49:09 +0545

    feat: :sparkles: searching the videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import { Search } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
-        borderRadius: "20",
+        borderRadius: 20,
         border: "1px solid #e3e3e3",
         pl: 2,
         boxShadow: "none",
@@ -18,12 +31,16 @@ const SearchBar = () => {
     >
       <input
         className="search-bar"
-        placeholder="Search.."
-        value=""
-        onChange={() => {}}
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
-        <Search />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
       </IconButton>
     </Paper>
   );
